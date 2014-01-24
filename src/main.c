0f6931dff21555413a014147bf9bb5a74f8bf4a8
Edwin Finch <edwinfinch8@gmail.com>
2014-01-24 15:08:17 -0800

    Fixed pebble resets, took out arrows
    Long story short, after much crap I just took Chris's code and copy and pasted it into my own project and changed the font back to mine. This removed the arrows and menu icon.

--- src/main.c
+++ src/main.c
@@ -1,95 +1,78 @@
 #include <pebble.h>
-
-Window *window;
+ 
+Window* window;
 TextLayer *text_layer;
-
-GBitmap *future_bitmap, *past_bitmap;
-BitmapLayer *future_layer, *past_layer;
+InverterLayer *inv_layer;
 
 char buffer[] = "00:00";
 
-InverterLayer *inv_layer;
-
-void window_load(Window *window)
-{	
-  text_layer = text_layer_create(GRect(0, 53, 144, 168));
-  text_layer_set_background_color(text_layer, GColorClear);
-  text_layer_set_text_color(text_layer, GColorBlack);
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
-  layer_add_child(window_get_root_layer(window), (Layer*) text_layer);
-  //I'm a retard
-  //I always feel like Katharine is watching over me
-  //Knowing that I'm terrible at this
-  //Mocking me
-	
-  inv_layer = inverter_layer_create(GRect(0, 50, 144, 62));
-  layer_add_child(window_get_root_layer(window), (Layer*) inv_layer);
-	
-  //Load bitmaps into GBitmap structures
-  //The ID you chose when uploading is prefixed with 'RESOURCE_ID_'
-  future_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FUTURE);
-  past_bitmap = gbitmap_create_with_resource(RESOURCE_ID_PAST);
- 
-  //Create BitmapLayers to show GBitmaps and add to Window
-  //Sample images are 144 x 50 pixels
-  future_layer = bitmap_layer_create(GRect(0, 0, 144, 50));
-  bitmap_layer_set_bitmap(future_layer, future_bitmap);
-  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(future_layer));
- 
-  past_layer = bitmap_layer_create(GRect(0, 112, 144, 50));
-  bitmap_layer_set_bitmap(past_layer, past_bitmap);
-  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(past_layer));
-}
-
 void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
-   //Format the buffer string using tick_time as the time source
-   strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
- 
-   //Change the TextLayer text to show the new time!
-   text_layer_set_text(text_layer, buffer);
+        //Format the buffer string using tick_time as the time source
+        strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+        
+        //Change the TextLayer text to show the new time!
+        text_layer_set_text(text_layer, buffer);
 }
-    
-
-
+ 
+void window_load(Window *window)
+{
+        //Time layer
+        text_layer = text_layer_create(GRect(0, 53, 132, 168));
+        text_layer_set_background_color(text_layer, GColorClear);
+        text_layer_set_text_color(text_layer, GColorBlack);
+        text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+        text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
+        
+        layer_add_child(window_get_root_layer(window), (Layer*) text_layer);
+        
+        //Inverter layer
+        inv_layer = inverter_layer_create(GRect(0, 50, 144, 62));
+        layer_add_child(window_get_root_layer(window), (Layer*) inv_layer);
+        
+        //Get a time structure so that the face doesn't start blank
+        struct tm *t;
+        time_t temp;        
+        temp = time(NULL);        
+        t = localtime(&temp);        
+        
+        //Manually call the tick handler when the window is loading
+        tick_handler(t, MINUTE_UNIT);
+}
+ 
 void window_unload(Window *window)
 {
-  text_layer_destroy(text_layer);
-  inverter_layer_destroy(inv_layer);
-  //Destroy GBitmaps
-  gbitmap_destroy(future_bitmap);
-  gbitmap_destroy(past_bitmap);
+        //We will safely destroy the Window's elements here!
+        text_layer_destroy(text_layer);
+        
+        inverter_layer_destroy(inv_layer);
+}
  
-  //Destroy BitmapLayers
-  bitmap_layer_destroy(future_layer);
-  bitmap_layer_destroy(past_layer);
-}
-
-void handle_init()
+void init()
 {
-	//Initialize the window
-  window = window_create();
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
-  window_stack_push(window, true);
-	//Push the window to front and center, m8ey
-  tick_timer_service_subscribe(MINUTE_UNIT, (TickHandler) tick_handler);
-	//subscribe to the time, updates on tick
+        //Initialize the app elements here!
+        window = window_create();
+        window_set_window_handlers(window, (WindowHandlers) {
+                .load = window_load,
+                .unload = window_unload,
+        });
+        
+        tick_timer_service_subscribe(MINUTE_UNIT, (TickHandler) tick_handler);
+        
+        window_stack_push(window, true);
 }
-
-void handle_deinit(void) {
-	//This is for deinitializing the window
-	  text_layer_destroy(text_layer);
-	  window_destroy(window);
+ 
+void deinit()
+{
+        //De-initialize elements here to save memory!
+        tick_timer_service_unsubscribe();
+        
+        window_destroy(window);
 }
-
-int main(void) {
-	//This is our main function, which returns nothing (void)
-	//It handles ticks, as well as initializing and deinitializing the window
-	  handle_init();
-	  app_event_loop();
-	  handle_deinit();
+ 
+int main(void)
+{
+        init();
+        app_event_loop();
+        deinit();
 }
