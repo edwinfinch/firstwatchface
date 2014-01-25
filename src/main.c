d4e40069a929a8dbc6a3dfc4a5f065524ee37709
Edwin Finch <edwinfinch8@gmail.com>
2014-01-25 10:49:08 -0800

    Added 12 hour support

--- src/main.c
+++ src/main.c
@@ -1,21 +1,47 @@
+/*
+Clean w/ battery and bluetooth
+Created by Edwin Finch
+Open source
+Feel free to modify, redistribute and do whatever you'd like with it :)
+
+Trek font taken from PebbleTrek by Kyle Potts: https://github.com/kylepotts/pebbletrek
+
+Note:
+I was in the middle of working on adding the date when I added 12/24hr functionality
+So please ignore/delete the lines that contain stuff about the date
+*/
+
 #include <pebble.h>
  
 Window* window;
 TextLayer *text_layer;
 TextLayer *battery_text_layer;
+							//TextLayer* date_text_layer;
 static GFont trek20;
 
 static GBitmap* bt_connected;
 static BitmapLayer* bt_connected_layer;
 
-char buffer[] = "00:00";
+char timeBuffer[] = "00:00";
+char dateBuffer[] = "14 August";
 
 void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
-  //Format the buffer string using tick_time as the time source
-  strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
-  //Change the TextLayer text to show the new time!
-  text_layer_set_text(text_layer, buffer);
+  //Format the Time buffer string using tick_time as the time source
+  strftime(timeBuffer, sizeof("00:00"), "%H:%M", tick_time);
+  //Format dateBuffer using tick_time as source
+ // strftime(dateBuffer, sizeof(dateBuffer), "%e %b", tick_time);
+	
+	if(clock_is_24h_style()){
+      strftime(timeBuffer, sizeof(timeBuffer), "%H:%M", tick_time);
+    }
+    else{
+      strftime(timeBuffer,sizeof(timeBuffer),"%I:%M",tick_time);
+    }
+	
+  //Change the TextLayer(s) text to show the new time and date
+  text_layer_set_text(text_layer, timeBuffer);
+  							//text_layer_set_text(date_text_layer, dateBuffer);
 }
  
 void handle_bt(bool connected){
@@ -54,6 +80,15 @@ void window_load(Window *window)
   text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));    
   layer_add_child(window_get_root_layer(window), (Layer*) text_layer);
 	
+  //Date layer (work in progress)
+  /*
+  date_text_layer = text_layer_create(GRect(120,0,40,40));
+  text_layer_set_font(date_text_layer,trek20);
+  text_layer_set_background_color(date_text_layer, GColorBlack);
+  text_layer_set_text_color(date_text_layer, GColorWhite);
+  layer_add_child(window_layer, text_layer_get_layer(date_text_layer));
+  */
+	
   //Battery text layer
   battery_text_layer = text_layer_create(GRect(0,145,40,40));
   text_layer_set_font(battery_text_layer,trek20);
@@ -67,7 +102,7 @@ void window_load(Window *window)
   bitmap_layer_set_background_color(bt_connected_layer,GColorClear);
   layer_add_child(window_layer,bitmap_layer_get_layer(bt_connected_layer));
 	
-  //Get a time structure so that the face doesn't start blank
+  //Get a time structure so that it doesn't start blank
   struct tm *t;
   time_t temp;        
   temp = time(NULL);        
@@ -75,10 +110,12 @@ void window_load(Window *window)
         
   //Manually call the tick handler when the window is loading
   tick_handler(t, MINUTE_UNIT);
-	
+
+  //Battery update handler
   BatteryChargeState btchg = battery_state_service_peek();
   handle_battery(btchg);
 	
+  //Bluetooth update handler
   bool connected = bluetooth_connection_service_peek();
   handle_bt(connected);
 
@@ -86,8 +123,9 @@ void window_load(Window *window)
  
 void window_unload(Window *window)
 {
-  //We will safely destroy the Window's elements here!
+  //Destroy elements
   text_layer_destroy(text_layer);
+  								//text_layer_destroy(date_text_layer);
   text_layer_destroy(battery_text_layer);
   fonts_unload_custom_font(trek20);
   bitmap_layer_destroy(bt_connected_layer);
@@ -96,7 +134,7 @@ void window_unload(Window *window)
  
 void init()
 {
- //Initialize the app elements here!
+ //Initialize the app elements
   window = window_create();
   window_set_window_handlers(window, (WindowHandlers) {
   .load = window_load,
@@ -111,7 +149,7 @@ void init()
  
 void deinit()
 {
-  //De-initialize elements here to save memory!
+  //De-initialize elements, saves memory
   tick_timer_service_unsubscribe();
   battery_state_service_unsubscribe();
   bluetooth_connection_service_unsubscribe();
