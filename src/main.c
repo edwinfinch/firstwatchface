812fe954fca3fed8ec346924ddb3733375b0ad00
Edwin Finch <edwinfinch8@gmail.com>
2014-02-07 14:56:01 -0800

    1.2: See changelog
    -Fixed time flying away and never coming back (I guess time really does fly.)
    -Fixed "animation" on init not animating at all
    -Added glance_this function
    ---Used to give the user an optional vibration and at a glance text dropdown
    -Added vibration on bluetooth disconnect/reconnect
    ---Added glance_this(); to bluetooth change

--- src/main.c
+++ src/main.c
@@ -9,12 +9,13 @@ Trek font taken from PebbleTrek by Kyle Potts: https://github.com/kylepotts/pebb
 
 #include <pebble.h>
 #define NUM_ANIMATION_PKEY 1
-#define NUM_ANIMATION_DEFAULT 0
+#define NUM_ANIMATION_DEFAULT 1
 	
 Window* window;
 TextLayer *text_layer;
 TextLayer *battery_text_layer;
 TextLayer* date_text_layer;
+TextLayer *update_at_a_glance;
 InverterLayer *invert_layer;
 static GFont trek20;
 
@@ -24,6 +25,10 @@ BitmapLayer* bt_connected_layer;
 char timeBuffer[] = "00:00";
 char dateBuffer[] = "February 31";
 
+bool booted = false;
+bool did58Animate = false;
+int animationNumber = 0;
+
 //Define our variables for persistent storage
 static int num_animation = NUM_ANIMATION_PKEY;
 //This is an int and not a bool because I'll be adding more colourschemes
@@ -32,6 +37,11 @@ void on_animation_stopped(Animation *anim, bool finished, void *context)
 {
     //Free the memoery used by the Animation
     property_animation_destroy((PropertyAnimation*) anim);
+	animationNumber++;
+	if(animationNumber > 3){
+		booted = true;
+		animationNumber = 0;
+	}
 }
  
 void animate_layer(Layer *layer, GRect *start, GRect *finish, int duration, int delay)
@@ -54,6 +64,34 @@ void animate_layer(Layer *layer, GRect *start, GRect *finish, int duration, int 
     animation_schedule((Animation*) anim);
 }
 
+//Custom function which takes a few variables pushes either a vibration, text update, or both to watch
+void glance_this(const char *updateText, bool vibrate, int vibrateNum, int animationLength){
+	//if the watchface has already initialized 
+	if(booted == true){
+		//Update the text layer to the char provided by function call
+		text_layer_set_text(update_at_a_glance, updateText);
+		//if there's a vibration request,
+		if(vibrate == true){
+			//check what number it is and fufill accordingly.
+			if(vibrateNum == 1){
+				vibes_short_pulse();
+		  }
+			else if(vibrateNum == 2){
+				vibes_double_pulse();
+		  }
+			else if(vibrateNum == 3){
+				vibes_long_pulse();
+		  }
+	  }
+		//Animate update_at_a_glance layer for fancy effect
+		GRect start01 = GRect(0, -100, 140, 168);
+		GRect finish01 = GRect(0, 10, 140, 168);
+		GRect start02 = GRect(0, 10, 140, 168);
+		GRect finish02 = GRect(0, -100, 140, 168);
+		animate_layer(text_layer_get_layer(update_at_a_glance), &start01, &finish01, 1000, 0);
+		animate_layer(text_layer_get_layer(update_at_a_glance), &start02, &finish02, 1000, animationLength);
+	}
+}
 
 void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
@@ -75,11 +113,11 @@ void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 	
   int seconds = tick_time->tm_sec;
         
-	if(seconds == 59)
+	if(seconds == 58 && booted == true)
         {
 		  srand(time(NULL));
 		  int whichAnimationTime = rand() % 5;
-		
+		  did58Animate = true;
 		
 		if(whichAnimationTime == 1){
           //Slide offscreen downward
@@ -117,24 +155,10 @@ void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 			GRect finish = GRect(300, 350, 140, 168);
             animate_layer(text_layer_get_layer(text_layer), &start, &finish, 500, 800);
 		}
-		/*
-		GRect start = GRect(0, 53, 140, 168);
-  		GRect finish = GRect(300, 250, 140, 168);
-  		GRect start1 = GRect(25, 97, 150, 140);
-  		GRect finish1 = GRect(-300, 250, 140, 168);
-  		GRect start2 = GRect(0, 145, 40, 40);
- 		GRect finish2 = GRect(300,-250, 40, 40);
- 		GRect start3 = GRect(115, 135, 40, 40);
-  		GRect finish3 = GRect(-300, -250, 40, 40);
-		animate_layer(text_layer_get_layer(text_layer), &start, &finish, 1200, 10);
-  		animate_layer(text_layer_get_layer(date_text_layer), &start1, &finish1, 1130, 10);
-  		animate_layer(text_layer_get_layer(battery_text_layer), &start2, &finish2, 1300, 10);
-  		animate_layer(bitmap_layer_get_layer(bt_connected_layer), &start3, &finish3, 1400, 10);
-		*/
 		
-        }
+      }
         
-    else if(seconds == 0)
+    else if(seconds == 0 && booted == true && did58Animate == true)
         {
           text_layer_set_text(text_layer, timeBuffer);
 		
@@ -165,44 +189,30 @@ void tick_handler(struct tm *tick_time, TimeUnits units_changed)
           GRect finish = GRect(0, 53, 140, 168);
           animate_layer(text_layer_get_layer(text_layer), &start, &finish, 500, 800);
 		}
-		/*
-		GRect start = GRect(300, 250, 140, 168);
-  		GRect finish = GRect(0, 53, 140, 168);
-  		GRect start1 = GRect(-300, 250, 140, 168);
-  		GRect finish1 = GRect(25, 97, 150, 140);
-  		GRect start2 = GRect(300,-250, 40, 40);
- 		GRect finish2 = GRect(0, 145, 40, 40);
- 		GRect start3 = GRect(-300, -250, 40, 40);
-  		GRect finish3 = GRect(115, 135, 40, 40);
-		animate_layer(text_layer_get_layer(text_layer), &start, &finish, 1200, 10);
-  		animate_layer(text_layer_get_layer(date_text_layer), &start1, &finish1, 1130, 10);
-  		animate_layer(text_layer_get_layer(battery_text_layer), &start2, &finish2, 1300, 10);
-  		animate_layer(bitmap_layer_get_layer(bt_connected_layer), &start3, &finish3, 1400, 10);
-		*/
-        }
+      }
         
     else
         {
-          //Change the TextLayer text to show the new time!
+          //Change the TextLayer text to show the new time
           text_layer_set_text(text_layer, timeBuffer);
-        }
+		if(seconds == 2 && did58Animate == true){
+			did58Animate = false;
+		}
+      }
 }
 
 void handle_bt(bool connected){
 	if(connected == 0){
-  	GRect start3 = GRect(115, -50, 40, 40);
-  	GRect finish3 = GRect(115, 135, 40, 40);
-  	animate_layer(bitmap_layer_get_layer(bt_connected_layer), &start3, &finish3, 1000, 0);
-  	bt_connected = gbitmap_create_with_resource(RESOURCE_ID_bt_disconnected);  
+		bt_connected = gbitmap_create_with_resource(RESOURCE_ID_bt_disconnected);  
+		glance_this("Bluetooth disconnected", 1, 3, 5000);
   }
-  if(connected == 1){
-	GRect start3 = GRect(115, -50, 40, 40);
-  	GRect finish3 = GRect(115, 135, 40, 40);
-  	animate_layer(bitmap_layer_get_layer(bt_connected_layer), &start3, &finish3, 1000, 0);
-  	bt_connected = gbitmap_create_with_resource(RESOURCE_ID_bt_connected);
+  	if(connected == 1){
+  	    bt_connected = gbitmap_create_with_resource(RESOURCE_ID_bt_connected);
+	    glance_this("Bluetooth connected", 1, 2, 3000);
   }
 	bitmap_layer_set_bitmap(bt_connected_layer,bt_connected);
 }
+
 
 void handle_battery(BatteryChargeState charge_state) {
   static char battery_text[] = "100%";
@@ -219,6 +229,13 @@ void window_load(Window *window)
 {
   Layer *window_layer = window_get_root_layer(window);
   trek20 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_trekfont_20));
+	
+  //Bluetooth layer
+  bt_connected = gbitmap_create_with_resource(RESOURCE_ID_bt_disconnected);
+  bt_connected_layer = bitmap_layer_create(GRect(115,135,40,40));
+  bitmap_layer_set_background_color(bt_connected_layer,GColorClear);
+  layer_add_child(window_layer,bitmap_layer_get_layer(bt_connected_layer));
+	
   //Time layer
   text_layer = text_layer_create(GRect(0, 53, 140, 168));
   text_layer_set_background_color(text_layer, GColorClear);
@@ -227,12 +244,21 @@ void window_load(Window *window)
   text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));    
   layer_add_child(window_get_root_layer(window), (Layer*) text_layer);
 	
+  //Quick update text layer
+  update_at_a_glance = text_layer_create(GRect(0, -50, 140, 168));
+  text_layer_set_background_color(update_at_a_glance, GColorClear);
+  text_layer_set_text_color(update_at_a_glance, GColorBlack);
+  text_layer_set_font(update_at_a_glance, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  layer_add_child(window_layer, (Layer*) update_at_a_glance);
+	
+	
   //Date layer
   date_text_layer = text_layer_create(GRect(25,97,150,140));
   text_layer_set_font(date_text_layer,fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
   text_layer_set_background_color(date_text_layer, GColorClear);
   text_layer_set_text_color(date_text_layer, GColorBlack);
   layer_add_child(window_layer, text_layer_get_layer(date_text_layer));
+
 
 	
   //Battery text layer
@@ -241,12 +267,6 @@ void window_load(Window *window)
   text_layer_set_background_color(battery_text_layer, GColorClear);
   text_layer_set_text_color(battery_text_layer, GColorBlack);
   layer_add_child(window_layer, text_layer_get_layer(battery_text_layer));
-    
-  //Bluetooth layer
-  bt_connected = gbitmap_create_with_resource(RESOURCE_ID_bt_disconnected);
-  bt_connected_layer = bitmap_layer_create(GRect(115,135,40,40));
-  bitmap_layer_set_background_color(bt_connected_layer,GColorClear);
-  layer_add_child(window_layer,bitmap_layer_get_layer(bt_connected_layer));
 	
   //Invert all pixels
   //Will now be white on black
@@ -280,6 +300,7 @@ void window_unload(Window *window)
   text_layer_destroy(text_layer);
   text_layer_destroy(date_text_layer);
   text_layer_destroy(battery_text_layer);
+  text_layer_destroy(update_at_a_glance);
   fonts_unload_custom_font(trek20);
   bitmap_layer_destroy(bt_connected_layer);
   inverter_layer_destroy(invert_layer);
@@ -304,12 +325,8 @@ void init()
 	//This is the old method of fetching an animation number
 	//Now it goes in order
 	
-	//srand(time(NULL));
-    //int whichAnimation;
-	//whichAnimation = (rand() % 5);
-	
 	if(num_animation > 5){
-		num_animation = NUM_ANIMATION_DEFAULT;
+		num_animation = 1;
 	}
 	
 	if(num_animation == 1){
@@ -375,7 +392,6 @@ void init()
   		animate_layer(text_layer_get_layer(battery_text_layer), &start2, &finish2, 1300, 10);
   		animate_layer(bitmap_layer_get_layer(bt_connected_layer), &start3, &finish3, 1400, 10);
 	}
-	
 	if(num_animation == 5){
 		GRect start = GRect(300, 250, 140, 168);
   		GRect finish = GRect(0, 53, 140, 168);
